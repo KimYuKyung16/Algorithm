d4f2f921bab68393f3e12e20a018dc47a4e6d20e
kyk <kyg0954@naver.com>
2022-11-16 23:57:45 +0900

    problem

--- SwExpertAcademy/Problem2001.py
+++ SwExpertAcademy/Problem2001.py
@@ -1,27 +1,42 @@
 T = int(input())
-
-# N: 배열 크기, M: 파리채 크기
+count = [0, 3, 6]
 
 for test_case in range(1, T + 1):
-    N, M = map(int, input().split())
     array = []
-    sum_result = 0
-    sum_list = []
-
-    for i in range(N):
+    result = 1
+    for i in range(9):
         input_list = list(map(int, input().split()))
         array.append(input_list)
 
-    for i in range(N-M+1):
-        for j in range(N-M+1):
-            for x in range(M):
-                sum_result += sum(list(map(int, array[i+x][j:j+M])))
-            sum_list.append(sum_result)
-            sum_result = 0
-    print('#{} {}'.format(test_case, max(sum_list)))
+    # square
+    for i in count:
+        if (result == 0):
+            break
+        for j in count:
+            square = set()
+            for x in range(1, 4):
+                square.update(array[i+(x-1)][j:j+3])
+            if (len(square) != 9):
+                result = 0
+                break
+            else:
+                result = 1
 
+    if result == 1:
+        rotated_array = [[0] * 9 for _ in range(9)]
+        # 90 rotate
+        for i in range(9):
+            for j in range(9):
+                rotated_array[j][9-i-1] = array[i][j]
 
+        # row & col
+        for i in range(9):
+            row = set(array[i][:9])
+            col = set(rotated_array[i][:9])
+            if (len(row) != 9 or len(col) != 9):
+                result = 0
+                break
+            else:
+                result = 1
 
-
-
-
+    print('#{} {}'.format(test_case, result))
