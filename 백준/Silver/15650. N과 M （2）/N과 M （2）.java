212a5f8647a93d5814a003fca82cb7ad07fa9042
KimYuKyung <kyg0954@gmail.com>
2024-01-31 14:53:31 +0900

    [Silver III] Title: N과 M (2), Time: 156 ms, Memory: 16260 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static int[] visited;
+	static int n, m;
+
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken()); // n까지의 수
+		m = Integer.parseInt(st.nextToken()); // m개를 고름
+		visited = new int[n + 1];
+
+		for (int i = 1; i < n + 1; i++) {
+			visited[i] = 1;
+			dfs(1, "" + i);
+			visited[i] = 0;
+		}
+	}
+
+	public static void dfs(int cnt, String str) {
+		if (cnt == m) {
+			System.out.println(str);
+			return;
+		}
+
+		for (int i = 1; i < n + 1; i++) {
+			if (visited[i] == 1)
+				continue;
+			if (i < str.charAt(str.length() - 1) - '0')
+				continue;
+			visited[i] = 1;
+			dfs(cnt + 1, str + " " + i);
+			visited[i] = 0;
+		}
+	}
+}
