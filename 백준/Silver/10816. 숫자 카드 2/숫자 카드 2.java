fe72e081237ab7877ea155a348e84702a6843b14
KimYuKyung <kyg0954@gmail.com>
2025-05-13 16:49:11 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 952 ms, Memory: 168388 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -4,43 +4,10 @@ import java.io.*;
 public class Main {
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   static StringTokenizer st;
-  static int[] cards1;
-
-  public static int get_first(int num) {
-    int left = 0;
-    int right = cards1.length;
-
-    while(left < right) {
-      int middle = (left + right) / 2;
-      if (cards1[middle] < num) {
-        left = middle + 1;
-      } else {
-        right = middle;
-      }
-    }
-
-    return left;
-  }
-
-  public static int get_last(int num) {
-    int left = 0;
-    int right = cards1.length;
-
-    while (left < right) {
-      int middle = (left + right) / 2;
-      if (cards1[middle] <= num) {
-        left = middle + 1;
-      } else {
-        right = middle;
-      }
-    }
-
-    return left;
-  }
 
   public static void main(String[] args) throws Exception {
     int N = Integer.parseInt(br.readLine());
-    cards1 = new int[N];
+    int[] cards1 = new int[N];
     st = new StringTokenizer(br.readLine());
     for (int i=0; i<N; i++) {
       cards1[i] = Integer.parseInt(st.nextToken());
@@ -53,13 +20,20 @@ public class Main {
       cards2[i] = Integer.parseInt(st.nextToken());
     }
 
-    Arrays.sort(cards1);
+    Map<Integer, Integer> list = new HashMap<>();
 
-    StringBuilder sb = new StringBuilder(); 
-    for (int num : cards2) {
-      int count = get_last(num) - get_first(num);
-      sb.append(count).append(" ");
+    for (int num : cards1) {
+      if (list.containsKey(num)) {
+        list.put(num, list.get(num) + 1);
+      } else {
+        list.put(num, 1);
+      }
     }
-    System.out.println(sb); 
+
+    StringBuilder sb = new StringBuilder();
+    for (int num : cards2) {
+      sb.append(list.get(num) != null ? list.get(num) : 0).append(" ");
+    }
+    System.out.println(sb);
   }
 }
