c85c3ded57168c320c464004292b626e7e7767c7
KimYuKyung <kyg0954@gmail.com>
2025-07-10 00:42:27 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,113 +1,74 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int n, m;
-	static Position robotP;
-	static int d;
-	static int[][] map;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int[] dy = {-1, 0, 1, 0};  // 북, 동, 남, 서
+    static int[] dx = {0, 1, 0, -1};
+    static int ry, rx, rd; // 로봇 상태
+    static int N, M;
+    static int[][] map;
 
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        int answer = 0;
 
-	static int answer = 0;
+        st = new StringTokenizer(br.readLine());
+        ry = Integer.parseInt(st.nextToken());
+        rx = Integer.parseInt(st.nextToken());
+        rd = Integer.parseInt(st.nextToken());
 
-	// 0은 북, 1은 동, 2는 남, 3은 서
-	// 1은 벽, 0은 청소된 빈칸, 2는 청소 완료
-	public static void main(String[] args) throws Exception {
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		robotP = new Position(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-		d = Integer.parseInt(st.nextToken()); // 청소기 방향
-		map = new int[n][m];
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j<M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < m; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        while(true) {
+            // 1.현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소
+            if (map[ry][rx] == 0) {
+                map[ry][rx] = 2;
+                answer++;
+            }
+            
+            boolean isClean = true;
+            for (int i=0; i<4; i++) {
+                // 3.현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 있는 경우
+                int ny = ry + dy[i];
+                int nx = rx + dx[i];
+                if (map[ny][nx] == 0) {
+                    isClean = false;
+                    // 3-1.반시계 방향으로 90도 회전
+                    rd = (rd + 3) % 4;
+                    // 3-2.바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진
+                    int nry = ry + dy[rd];
+                    int nrx = rx + dx[rd];
+                    if (map[nry][nrx] == 0) {
+                        ry = nry;
+                        rx = nrx;
+                    }
+                    break;
+                }
+            }
 
-		while (true) {
-			int rr = robotP.r;
-			int rc = robotP.c;
+            // 2. 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우
+            if (isClean) {
+                // 2-1.바라보는 방향을 유지한 채로 한 칸 후진할 수 있다면 한 칸 후진하고 1번으로 돌아감.
+                // 2-2.바라보는 방향의 뒤쪽 칸이 벽이라 후진할 수 없다면 작동을 멈춤.
+                int ny = ry - dy[rd];
+                int nx = rx - dx[rd];
+                if (ny < 0 || ny >= N || nx < 0 || nx >= M) break;
+                if (map[ny][nx] == 1) break;
+                ry = ny;
+                rx = nx;
+            }
+        }
 
-			// 청소 안됐으면 청소
-			if (map[rr][rc] == 0) {
-				map[rr][rc] = 2;
-				answer++;
-			}
-
-			// 청소할 칸이 있는 경우
-			if (confirm_around()) {
-				while (true) {
-					// 반시계 방향으로 90도 회전
-					if (d == 0) {
-						d = 3;
-					} else {
-						d = d - 1;
-					}
-
-					int nr = robotP.r + dr[d];
-					int nc = robotP.c + dc[d];
-
-					if (nr < 0 || nr >= n || nc < 0 || nc >= m)
-						continue;
-					if (map[nr][nc] == 0) {
-						robotP.r = nr;
-						robotP.c = nc;
-						break;
-					}
-				}
-			} else { // 청소할 칸이 없는 경우
-				// 한 칸 후친할 수 있으면 후진
-				int nd = (d + 2) % 4; // 후진할 방향
-				int nr = robotP.r + dr[nd];
-				int nc = robotP.c + dc[nd];
-
-				if (nr < 0 || nr >= n || nc < 0 || nc >= m) {
-					// 후진 불가능할 경우
-					System.out.println(answer);
-					return;
-				}
-				if (map[nr][nc] == 1) {
-					// 후진 불가능할 경우
-					System.out.println(answer);
-					return;
-				}
-				robotP.r = nr;
-				robotP.c = nc;
-			}
-		}
-	}
-
-	// 주변에 청소할 칸이 있는지 확인
-	public static boolean confirm_around() {
-		for (int i = 0; i < 4; i++) {
-			int nr = robotP.r + dr[i];
-			int nc = robotP.c + dc[i];
-
-			if (nr < 0 || nr >= n || nc < 0 || nc >= m)
-				continue;
-			if (map[nr][nc] == 0) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static class Position {
-		int r;
-		int c;
-
-		public Position(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-	}
+        System.out.println(answer);
+    }
 }
