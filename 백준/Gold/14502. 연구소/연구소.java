44f5d05880dccdaf641d2e22cd6c2b56366d9967
KimYuKyung <kyg0954@gmail.com>
2025-07-15 17:45:31 +0900

    [Gold IV] Title: 연구소, Time: 920 ms, Memory: 299648 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,137 +1,108 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int n, m;
-	static int[][] map;
-	static int[][] nMap;
-	static ArrayList<Position> virus = new ArrayList<Position>();
-	static int[] dy = { -1, 0, 1, 0 };
-	static int[] dx = { 0, 1, 0, -1 };
-	static int answer = Integer.MIN_VALUE;
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static StringTokenizer st;
+  static int N, M;
+  static int[][] map;
+  static int[] dy = {-1,0,1,0};
+  static int[] dx = {0,1,0,-1};
+  static ArrayList<Node> viruses = new ArrayList<>();
+  static int answer = 0;
 
-	public static void main(String[] args) throws Exception {
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
-		nMap = new int[n][m];
+  // 0은 빈칸, 1은 벽, 2는 바이러스
+  public static void main(String[] args) throws Exception {
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    map = new int[N][M];
 
-		for (int y = 0; y < n; y++) {
-			st = new StringTokenizer(br.readLine());
-			for (int x = 0; x < m; x++) {
-				map[y][x] = Integer.parseInt(st.nextToken());
+    for (int i=0; i<N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j=0; j<M; j++) {
+        int c = Integer.parseInt(st.nextToken());
+        if (c == 2) {
+          viruses.add(new Node(i, j));
+        }
+        map[i][j] = c;
+      }
+    }
 
-				if (map[y][x] == 2) {
-					virus.add(new Position(y, x));
-				}
-			}
-		}
+    dfs(0);
 
-		for (int y = 0; y < n; y++) {
-			for (int x = 0; x < m; x++) {
-				if (map[y][x] == 0) {
-					copy_map();
+    System.out.println(answer);
+  }
 
-					map[y][x] = 1;
-					dfs(1);
-					map[y][x] = 0;
-				}
-			}
-		}
+  public static void dfs(int count) {
+    if (count == 3) {
+      bfs();
+      return;
+    }
 
-		System.out.println(answer);
-	}
+    for (int i=0; i<N; i++) {
+      for (int j=0; j<M; j++) {
+        if (map[i][j] == 0) {
+          map[i][j] = 1;
+          dfs(count+1);
+          map[i][j] = 0;
+        }
+      }
+    }
+  }
 
-	public static void dfs(int cnt) {
-		if (cnt == 3) {
-			copy_map();
-			bfs();
-			return;
-		}
+  public static void bfs() {
+    Queue<Node> queue = new LinkedList<>();
+    int[][] nMap = new int[N][M];
+    for (int i = 0; i < N; i++) {
+        nMap[i] = map[i].clone(); 
+    }
 
-		for (int y = 0; y < n; y++) {
-			for (int x = 0; x < m; x++) {
-				if (map[y][x] == 0) {
-					map[y][x] = 1;
-					dfs(cnt + 1);
-					map[y][x] = 0;
-				}
-			}
-		}
-	}
+    for (Node n : viruses) {
+      queue.add(n);
+    }
 
-	public static void bfs() {
-		Queue<Position> q = new LinkedList<Position>();
-		boolean[][] visited = new boolean[n][m];
+    while(!queue.isEmpty()) {
+      Node current = queue.poll();
 
-		for (int i = 0; i < virus.size(); i++) {
-			q.add(virus.get(i));
-		}
+      for (int i=0; i<4; i++) {
+        int ny = current.y + dy[i];
+        int nx = current.x + dx[i];
 
-		while (!q.isEmpty()) {
-			Position current = q.remove();
-			int y = current.y;
-			int x = current.x;
+        if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+        if (nMap[ny][nx] == 1 || nMap[ny][nx] == 2) continue;
 
-			for (int i = 0; i < 4; i++) {
-				int ny = y + dy[i];
-				int nx = x + dx[i];
+        nMap[ny][nx] = 2;
+        queue.add(new Node(ny, nx));
+      }
+    }
 
-				if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
-					continue;
-				}
-				if (nMap[ny][nx] == 1 || nMap[ny][nx] == 2) {
-					continue;
-				}
-				if (visited[ny][nx]) {
-					continue;
-				}
+    confirm(nMap);
+  }
 
-				nMap[ny][nx] = 2;
-				visited[ny][nx] = true;
-				q.add(new Position(ny, nx));
-			}
-		}
+  public static void confirm(int[][] mapResult) {
+    int count = 0;
+    for (int i=0; i<N; i++) {
+      for (int j=0; j<M; j++) {
+        if (mapResult[i][j] == 0) {
+          count++;
+        }
+      }
+    }
+    answer = Math.max(answer, count);
+  }
 
-		confirm();
-	}
+  public static class Node {
+    int y, x;
 
-	public static void confirm() {
-		int cnt = 0;
+    Node(int y, int x) {
+      this.y = y;
+      this.x = x;
+    }
 
-		for (int y = 0; y < n; y++) {
-			for (int x = 0; x < m; x++) {
-				if (nMap[y][x] == 0) {
-					cnt++;
-				}
-			}
-		}
-
-		answer = Math.max(answer, cnt);
-	}
-
-	public static void copy_map() {
-		for (int y = 0; y < n; y++) {
-			for (int x = 0; x < m; x++) {
-				nMap[y][x] = map[y][x];
-			}
-		}
-	}
-
-	public static class Position {
-		int y;
-		int x;
-
-		public Position(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-	}
+    public String toString() {
+      return "(" + y + "," + x + ")";
+    }
+  }
+  
 }
