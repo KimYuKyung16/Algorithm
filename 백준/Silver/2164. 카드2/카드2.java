05a6776332875d09ac3fa9201ba99adec5f858a7
KimYuKyung <kyg0954@gmail.com>
2025-07-16 03:27:18 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 45728 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,34 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static BufferedReader br;
-	static Queue<Integer> queue = new LinkedList<>();
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws Exception {
+    int N = Integer.parseInt(br.readLine());
+    
+    Deque<Integer> deque = new LinkedList<>();
+    for (int i=1; i<=N; i++) {
+      deque.add(i);
+    }
 
-	public static void main(String[] args) throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+    while(deque.size() > 1) {
+      deque.pollFirst();
+      deque.addLast(deque.pollFirst());
+    } 
 
-		for (int i = 1; i <= n; i++) {
-			queue.add(i);
-		}
-
-		int index = 0;
-		while (true) {
-			if (queue.size() == 1)
-				break;
-
-			if (index % 2 == 0) {
-				queue.remove();
-			} else {
-				queue.add(queue.remove());
-			}
-			index++;
-		}
-
-		System.out.println(queue.peek());
-	}
+    System.out.println(deque.peek());
+  }
 }
