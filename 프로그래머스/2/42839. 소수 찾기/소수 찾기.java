c642bd863bfe0376837d1599564514fe435fca1a
KimYuKyung <kyg0954@gmail.com>
2025-09-26 16:20:41 +0900

    [level 2] Title: 소수 찾기, Time: 29.17 ms, Memory: 96.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    static char[] nums;
+    static int len;
+    static Set<Integer> set = new HashSet<>();
+    
+    public int solution(String numbers) {
+        int answer = 0;
+        nums = numbers.toCharArray();
+        len = nums.length;
+        
+        dfs(0, "", new boolean[len]);
+        
+        for (int n : set) {
+            if (isPrime(n)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public static boolean isPrime(int n) {
+        if (n < 2) return false; 
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    
+    public void dfs(int index, String str, boolean[] visited) {
+        if (!str.equals("")) {
+            set.add(Integer.parseInt(str));
+        }
+        
+        for (int i=0; i<len; i++) {
+            if (visited[i]) continue;
+            
+            visited[i] = true;
+            dfs(index+1, str + nums[i], visited);
+            visited[i] = false;
+        }
+        
+    }
+}
