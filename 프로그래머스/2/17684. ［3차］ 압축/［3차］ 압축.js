2c3d304d564b189249ba025ddbcc7d2349cc65fe
KimYuKyung <kyg0954@gmail.com>
2025-10-09 18:32:31 +0900

    [level 2] Title: [3차] 압축, Time: 0.95 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,35 +1,4 @@
 function solution(msg) {
-    const answer = [];
-    const dic = {};
-    
-    for (let i=65; i<=65+25; i++) {
-        const num = String.fromCharCode(i);
-        dic[num] = i-64;
-    }
-    
-    let dicIdx = 27;
-    for(let i=0; i<msg.length; i++) {
-        let index = i;
-        let str = msg[index];
-        
-        while(true) {
-            const isDic = dic[str];
-            if (isDic) {
-                if (index+1 >= msg.length) {
-                    answer.push(dic[str]);
-                    return answer
-                }
-                str += msg[++index];
-            } else {
-                answer.push(dic[str.slice(0, str.length-1)]);
-                if (dic[str]) break;
-                dic[str] = dicIdx;
-                break;
-            }
-        }
-        i = index-1;
-        dicIdx++;
-    }
-    
+    var answer = [];
     return answer;
 }
