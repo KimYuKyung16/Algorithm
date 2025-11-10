3a0a88817fd8f04267e8588ba680ef454bc037a6
KimYuKyung <kyg0954@gmail.com>
2025-11-10 17:54:25 +0900

    [Gold IV] Title: 최단경로, Time: 640 ms, Memory: 107952 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,32 +1,19 @@
 import java.io.*;
 import java.util.*;
 
-class Node {
-  int v;
-  int d;
-
-  Node(int v, int d) {
-    this.v = v;
-    this.d = d;
-  }
-}
-
 public class Main {
-  static BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
-  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   static StringTokenizer st;
   static List<List<Node>> graph = new ArrayList<>();
-  static int[] distances;
+  static int V,E,K;
 
   public static void main(String[] args) throws Exception {
     st = new StringTokenizer(br.readLine());
-    int V = Integer.parseInt(st.nextToken());
-    int E = Integer.parseInt(st.nextToken());
-    int K = Integer.parseInt(br.readLine());
-    distances = new int[V+1];
-    Arrays.fill(distances, Integer.MAX_VALUE);
-
-    for (int i = 0; i <= V; i++) {
+    V = Integer.parseInt(st.nextToken());
+    E = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(br.readLine()); // 시작 번호
+    
+    for (int i=0; i<=V; i++) {
       graph.add(new ArrayList<>());
     }
 
@@ -34,48 +21,57 @@ public class Main {
       st = new StringTokenizer(br.readLine());
       int u = Integer.parseInt(st.nextToken());
       int v = Integer.parseInt(st.nextToken());
-      int w = Integer.parseInt(st.nextToken()); // 가중치
-
+      int w = Integer.parseInt(st.nextToken());
+    
       graph.get(u).add(new Node(v, w));
     }
 
     dijkstra(K);
-
-    StringBuilder sb = new StringBuilder();
-    for (int i=1; i<distances.length; i++) {
-      if (distances[i] == Integer.MAX_VALUE) {
-        sb.append("INF").append("\n");
-      } else {
-        sb.append(distances[i]).append("\n");
-      }
-    }
-    bw.write(sb.toString());
-    bw.flush();
-    bw.close();
   }
 
-  public static void dijkstra(int start) {
-    PriorityQueue<Node> queue = new PriorityQueue<>((a, b) -> {
-      return Integer.compare(a.d, b.d);
-    });
-    distances[start] = 0;
-    queue.add(new Node(start, 0));
+  public static void dijkstra(int s) {
+    PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.w - b.w);
+    int[] dists = new int[V+1];
+    Arrays.fill(dists, Integer.MAX_VALUE);
+    pq.add(new Node(s, 0));
+    dists[s] = 0;
 
-    while (!queue.isEmpty()) {
-      Node current = queue.poll();
-      int currentV = current.v;
-      int currentD = current.d;
+    while(!pq.isEmpty()) {
+      Node c = pq.poll();
+      int cv = c.v;
+      int cw = c.w;
 
-      if (currentD > distances[currentV]) continue;
+      if (cw > dists[cv]) continue;
 
-      for (Node neighbor : graph.get(currentV)) {
-        int newDist = distances[currentV] + neighbor.d;
-
-        if (newDist < distances[neighbor.v]) {
-          distances[neighbor.v] = newDist;
-          queue.add(new Node(neighbor.v, newDist));
-        }
+      for (Node n : graph.get(cv)) {
+        int newCost = cw + n.w;
+        if (newCost < dists[n.v]) {
+          dists[n.v] = newCost;
+          pq.add(new Node(n.v, newCost));
+        } 
       }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int i=1; i<dists.length; i++) {
+      if (dists[i] == Integer.MAX_VALUE) {
+        sb.append("INF");
+      } else {
+        sb.append(dists[i]);
+      }
+      sb.append('\n');
+    }
+
+    System.out.println(sb.toString());
+  }
+
+  private static class Node {
+    int v;
+    int w;
+
+    Node(int v, int w) {
+      this.v = v;
+      this.w = w;
     }
   }
 }
