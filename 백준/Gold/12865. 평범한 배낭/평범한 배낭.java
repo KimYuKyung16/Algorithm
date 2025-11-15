358f9258dac891313eee700c1e78b355d79013d4
KimYuKyung <kyg0954@gmail.com>
2025-11-15 19:04:37 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 15504 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,13 +1,16 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   static StringTokenizer st;
+
   public static void main(String[] args) throws Exception {
     st = new StringTokenizer(br.readLine());
     int N = Integer.parseInt(st.nextToken());
     int K = Integer.parseInt(st.nextToken());
     int[] dp = new int[K+1];
+
     for (int i=0; i<N; i++) {
       st = new StringTokenizer(br.readLine());
       int W = Integer.parseInt(st.nextToken());
@@ -16,7 +19,8 @@ public class Main {
       for (int j=K; j>=W; j--) {
         dp[j] = Math.max(dp[j], dp[j-W] + V);
       }
-    }
+    } 
+
     System.out.println(dp[K]);
   }
 }
